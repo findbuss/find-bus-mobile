df45031c52f57d8c686f712d800317d2efaef697
douglasdans <douglas.dans@gmail.com>
2024-11-03 11:38:34 -0300

    feat: adiciona mapa

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,29 +1,37 @@
-import { Text, View, StyleSheet } from 'react-native'
-import Wrapper from '../components/Wrapper'
-import SearchBar from '../components/SearchBar'
-import Bus from '../components/Bus'
-import colors from '../styles/colors'
+import { Text, View, StyleSheet } from "react-native";
+import Wrapper from "../components/Wrapper";
+import SearchBar from "../components/SearchBar";
+import Bus from "../components/Bus";
+import colors from "../styles/colors";
+import MapView from "react-native-maps";
 
 export default function Map() {
   return (
     <Wrapper>
       <View style={styles.container}>
-        <SearchBar/>
+        <SearchBar />
+        <MapView style={styles.map} />
       </View>
     </Wrapper>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
+    display: "flex",
     gap: 12,
-    height: '100%',
-    justifyContent: 'space-between',
-    padding: 12
+    height: "100%",
+    justifyContent: "space-between",
+    padding: 12,
+    flex: 1,
   },
   title: {
     color: colors.primaryTextColor,
-    fontWeight: 'bold'
-  }
-})
+    fontWeight: "bold",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+  },
+});
