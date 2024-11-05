e67bc113807182ea2b1fa1304d68c55e9cc6d31f
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2024-11-04 23:57:28 -0300

    Criação da interface da barra de pesquisa

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,37 @@
 import { useState } from 'react'
-import { View, TextInput, StyleSheet } from 'react-native'
+import { View, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import colors from '../styles/colors'
-import ProfileButton from '../components/ProfileButton'
+import ProfileButton from './ProfileButton'
 
-export default function SearchBar({ navigation }) {
+export default function SearchBar({ children, navigation }) {
   const [isClicked, setIsClicked] = useState(false)
 
   return (
-    <View style={isClicked ? styles.selectedContainer : styles.container}>
-      <Ionicons style={styles.icon} name="search-outline"/>
-      <TextInput
-        style={styles.input}
-        placeholder="Pesquisar"
-        onFocus={() => {
-          
-        }}
-      />
-      <ProfileButton navigation={navigation}/>
-    </View>
+    <>
+      <View style={isClicked ? styles.selectedContainer : styles.container}>
+        <Ionicons style={styles.icon} name="search-outline"/>
+        <TextInput
+          style={styles.input}
+          placeholder="Pesquisar"
+          onFocus={() => setIsClicked(true)}
+        />
+        <ProfileButton navigation={navigation}/>
+      </View>
+      {isClicked && <View style={styles.search}>
+        <View style={styles.container}>
+          <TouchableOpacity onPress={() => setIsClicked(false)}>
+            <Ionicons style={styles.icon} name="arrow-back-outline"/>
+          </TouchableOpacity>
+          <TextInput
+            style={styles.input}
+            placeholder="Pesquisar"
+            autoFocus={true}
+          />
+        </View>
+        {children}
+      </View>}
+    </>
   )
 }
 
@@ -33,7 +46,7 @@ const styles = StyleSheet.create({
     gap: 8,
     outline: 'auto',
     outlineColor: colors.highlightColor,
-    paddingInline: 12,
+    paddingHorizontal: 12,
     width: '100%'
   },
   container: {
@@ -44,7 +57,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     gap: 8,
-    paddingInline: 12,
+    paddingHorizontal: 12,
     width: '100%'
   },
   icon: {
@@ -55,5 +68,17 @@ const styles = StyleSheet.create({
     paddingRight: 12,
     paddingVertical: 12,
     width: '100%'
+  },
+  search: {
+    backgroundColor: colors.primaryBackgroundColor,
+    flex: 1,
+    gap: 12,
+    height: '100%',
+    left: 0,
+    padding: 12,
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    zIndex: 1
   }
 })
