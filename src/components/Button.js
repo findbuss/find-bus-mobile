9c93f315ea9fe21c9c821adede3ae4a5662875a0
felipe-coletti <felipecoletti2@gmail.com>
2024-11-30 20:34:37 -0300

    feat: Adiciona o sistema de variantes ao componente botão

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,37 @@
 import { TouchableOpacity, Text, StyleSheet } from 'react-native'
 import colors from '../styles/colors'
 
-export default function Button({ children, ...rest }) {
+export default function Button({ children, variant = 'primary', ...rest }) {
+    let buttonBackgroundColor
+    let buttonTextColor
+
+    switch (variant) {
+        case 'primary':
+            buttonBackgroundColor = colors.highlightColor
+            buttonTextColor = colors.secondaryBackgroundColor
+            break
+        case 'secondary':
+            buttonBackgroundColor = colors.tertiaryBackgroundColor
+            buttonTextColor = colors.primaryTextColor
+            break
+        case 'error':
+            buttonBackgroundColor = colors.negativeColor
+            buttonTextColor = colors.secondaryBackgroundColor
+    }
+
+    const buttonStyle = {
+        ...styles.button,
+        backgroundColor: buttonBackgroundColor
+    }
+
+    const textStyle = {
+        ...styles.title,
+        color: buttonTextColor
+    }
+
     return (
-        <TouchableOpacity style={styles.button} {...rest}>
-            <Text style={styles.title}>{children}</Text>
+        <TouchableOpacity style={buttonStyle} {...rest}>
+            <Text style={textStyle}>{children}</Text>
         </TouchableOpacity>
     )
 }
@@ -12,13 +39,11 @@ export default function Button({ children, ...rest }) {
 const styles = StyleSheet.create({
     button: {
         alignItems: 'center',
-        backgroundColor: colors.highlightColor,
         borderRadius: 12,
         justifyContent: 'center',
         padding: 12
     },
     title: {
-        color: colors.secondaryBackgroundColor,
         fontSize: 14,
         fontWeight: 'bold'
     }
