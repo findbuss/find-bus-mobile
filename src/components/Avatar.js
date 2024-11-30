658f5c1039d5074a13461f1dab64a8ef2f17dc1f
felipe-coletti <felipecoletti2@gmail.com>
2024-11-30 01:07:44 -0300

    style: simplifica o código

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -5,67 +5,65 @@ import Ionicons from 'react-native-vector-icons/Ionicons'
 import * as SecureStore from 'expo-secure-store'
 
 export default function Avatar({ navigation }) {
-  const username = 'João'
+    const username = 'João'
 
-  const [userId, setUserId] = useState(null)
+    const [userId, setUserId] = useState(null)
 
-  const loadUserId = async () => {
-    try {
-      const storedUserId = await SecureStore.getItemAsync('user_id')
+    const loadUserId = async () => {
+        try {
+            const storedUserId = await SecureStore.getItemAsync('user_id')
 
-      if (storedUserId) {
-        setUserId(storedUserId)
-      } else {
-        console.log('Nenhuma credencial encontrada no Secure Store')
-      }
-    } catch (error) {
-      console.error('Erro ao recuperar o ID do usuário', error)
+            if (storedUserId) {
+                setUserId(storedUserId)
+            } else {
+                console.log('Nenhuma credencial encontrada no Secure Store')
+            }
+        } catch (error) {
+            console.error('Erro ao recuperar o ID do usuário', error)
+        }
     }
-  }
 
-  useEffect(() => {
-    loadUserId()
-  }, [])
+    useEffect(() => {
+        loadUserId()
+    }, [])
 
-  return (
-    userId ? (
-      <TouchableOpacity style={styles.background} onPress={() => navigation.navigate('Profile')}>
-        <Text style={styles.defaultText}>{username[0]}</Text>
-      </TouchableOpacity>
+    return userId ? (
+        <TouchableOpacity style={styles.background} onPress={() => navigation.navigate('Profile')}>
+            <Text style={styles.defaultText}>{username[0]}</Text>
+        </TouchableOpacity>
     ) : (
-      <TouchableOpacity style={styles.container} onPress={() => navigation.navigate('SignIn')}>
-        <Ionicons style={styles.icon} name="person" />
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.container} onPress={() => navigation.navigate('SignIn')}>
+            <Ionicons style={styles.icon} name="person" />
+        </TouchableOpacity>
     )
-  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    aspectRatio: '1/1',
-    backgroundColor: colors.tertiaryBackgroundColor,
-    borderRadius: '50%',
-    display: 'flex',
-    height: 32,
-    justifyContent: 'center'
-  },
-  background: {
-    alignItems: 'center',
-    aspectRatio: '1/1',
-    backgroundColor: colors.highlightColor,
-    borderRadius: '50%',
-    display: 'flex',
-    height: 32,
-    justifyContent: 'center'
-  },
-  defaultText: {
-    color: colors.secondaryBackgroundColor,
-    fontSize: 16,
-    fontWeight: 'bold'
-  },
-  icon: {
-    color: colors.secondaryTextColor,
-    fontSize: 16
-  }
+    container: {
+        alignItems: 'center',
+        aspectRatio: '1/1',
+        backgroundColor: colors.tertiaryBackgroundColor,
+        borderRadius: '50%',
+        display: 'flex',
+        height: 32,
+        justifyContent: 'center'
+    },
+    background: {
+        alignItems: 'center',
+        aspectRatio: '1/1',
+        backgroundColor: colors.highlightColor,
+        borderRadius: '50%',
+        display: 'flex',
+        height: 32,
+        justifyContent: 'center'
+    },
+    defaultText: {
+        color: colors.secondaryBackgroundColor,
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    icon: {
+        color: colors.secondaryTextColor,
+        fontSize: 16
+    }
 })
--- src/components/Link.js
+++ src/components/Link.js
@@ -1,20 +1,18 @@
 import { TouchableWithoutFeedback, Text, StyleSheet } from 'react-native'
 import colors from '../styles/colors'
 
-export default function Link({ children, to, navigation}) {
-  return (
-    <TouchableWithoutFeedback style={styles.link} onPress={() => navigation.navigate(to)}>
-      <Text style={styles.linkText}>
-        {children}
-      </Text>
-    </TouchableWithoutFeedback>
-  )
+export default function Link({ children, to, navigation }) {
+    return (
+        <TouchableWithoutFeedback style={styles.link} onPress={() => navigation.navigate(to)}>
+            <Text style={styles.linkText}>{children}</Text>
+        </TouchableWithoutFeedback>
+    )
 }
 
 const styles = StyleSheet.create({
-  linkText: {
-    color: colors.highlightColor,
-    fontSize: 14,
-    textAlign: 'center'
-  }
+    linkText: {
+        color: colors.highlightColor,
+        fontSize: 14,
+        textAlign: 'center'
+    }
 })
