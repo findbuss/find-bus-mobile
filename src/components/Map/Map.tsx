d1196adc0d0db090662357bf49ec166400f45605
felipe-coletti <felipecoletti2@gmail.com>
2025-06-02 14:06:51 -0300

    feat: adiciona o mapa

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,5 +1,25 @@
-import { View } from 'react-native'
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
+import { MapProps } from './Map.types'
 
-export function Map() {
-    return <View></View>
+export function Map({ latitude, longitude }: MapProps) {
+	const initialRegion = {
+		latitude: -23.55052,
+		longitude: -46.633308,
+		latitudeDelta: 0.0922,
+		longitudeDelta: 0.0421
+	}
+
+	return (
+		<MapView style={styles.map} initialRegion={initialRegion}>
+			<Marker coordinate={{ latitude: latitude, longitude: longitude }} />
+		</MapView>
+	)
 }
+
+const styles = StyleSheet.create({
+	map: {
+		flex: 1
+	}
+})
